316cc5e8370cbad6aaa81a8b2e6bef10b0ce7b19
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2015-09-18 22:09:25 +0300

    Added Limelight

---
+++ lib/url_tokenizer/limelight.rb
@@ -0,0 +1,77 @@
+require 'uri'
+require 'cgi'
+require 'digest'
+require_relative 'provider'
+
+module UrlTokenizer
+  class Limelight < Provider
+    PARAMS = %i[e h p cf cd]
+
+    def call(input_url, **options)
+      uri = URI.parse input_url
+      folder = File.dirname input_url
+
+      provider_options = build_options options, url: folder, uri: uri
+
+      query_string = URI.encode_www_form provider_options
+      url_to_encode = [folder, query_string].join '?'
+
+      provider_options[:h] = digest url_to_encode
+
+      uri.query = URI.encode_www_form provider_options
+      uri.to_s
+    end
+
+    private
+    def build_options(token_options, url:, uri:)
+      server_params = {
+        p: url.length,
+        e: expiration_date(token_options[:expire_in]),
+      }.delete_if { |k, v| v.nil? }
+
+      cookie_params = {
+        cd: token_options[:cd],
+        cf: expiration_date(token_options[:cf]),
+      }.delete_if { |k, v| v.nil? }
+
+      server_params.delete :e if cookie_params.any?
+
+      url_params(uri)
+        .merge(server_params)
+        .merge(cookie_params)
+    end
+
+    def url_params(uri)
+      return {} if uri.query.nil?
+
+      symbolize_keys(CGI.parse(uri.query)).tap do |params|
+        PARAMS.each do |param|
+          params.delete param
+        end
+      end
+    end
+
+    def expiration_date(expires_in)
+      Time.now.utc.to_i + expires_in.to_i if expires_in
+    end
+
+    def digest(url)
+      Digest::MD5.hexdigest "#{ key }#{ url }"
+    end
+
+    def symbolize_keys(hash)
+      hash.inject({}){|result, (key, value)|
+        new_key = case key
+                  when String then key.to_sym
+                  else key
+                  end
+        new_value = case value
+                    when Hash then symbolize_keys(value)
+                    else value
+                    end
+        result[new_key] = new_value
+        result
+      }
+    end
+  end
+end
---
+++ spec/lib/url_tokenizer/limelight_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+require 'url_tokenizer/limelight'
+
+describe UrlTokenizer::Limelight do
+  subject { described_class.new key }
+  let(:key) { "secret" }
+  let(:url) { url_with_params }
+
+  describe 'when url have the query string' do
+    let(:url) { url_with_params foo: :bar }
+
+    it 'does not add question mark' do
+      expect(subject.call(url).count('?')).to eq 1
+    end
+  end
+
+  describe 'when url have provider parameters' do
+    let(:url) { url_with_params e: 12345 }
+
+    it 'ignores and overwrites them' do
+      expect(subject.call url, expire_in: 10).not_to include 'e=12345'
+      expect(subject.call url, expire_in: 10).to match /e=\d+\&/
+    end
+  end
+
+  it "adds h parameter" do
+    expect(subject.call url).to match /&h=[a-z0-9]+\z/
+  end
+
+  it "does not include cookie params" do
+    expect(subject.call url).not_to include 'cf='
+    expect(subject.call url).not_to include 'cd='
+  end
+
+  describe 'cookie based auth' do
+    let(:url) { url_with_params }
+
+    it "adds cd parameter" do
+      expect(subject.call url, cd: 100).to include 'cd=100'
+    end
+
+    it "adds cf parameter" do
+      expect(subject.call url, cf: 100).not_to include 'cf=100'
+      expect(subject.call url, cf: 100).to match /cf=\d+\&/
+    end
+
+    it "does not add e parameter" do
+      expect(subject.call url, cd: 100, cf: 12345).not_to include 'e='
+    end
+
+    it "removes e parameter" do
+      url << '?e=12345'
+      expect(subject.call url, cd: 100, cf: 12345).not_to include 'e='
+    end
+  end
+
+  private
+  def url_with_params(**params)
+    query_string = URI.encode_www_form params
+    query_string = "?#{ query_string }" unless query_string.empty?
+    "http://liveplay.example.com/limelight/testtokenstream/playlist.m3u8#{ query_string }"
+  end
+end
