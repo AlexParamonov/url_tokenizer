387d5024293279ff6f95d8642a7adf4b7223c591
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2015-09-18 23:16:22 +0300

    symbolize keys

--- lib/url_tokenizer.rb
+++ lib/url_tokenizer.rb
@@ -4,18 +4,33 @@ module UrlTokenizer
   Error = Class.new StandardError
   class << self
     def provider(name)
-      providers.fetch(name) do
+      providers.fetch(name.to_sym) do
         raise Error, "unknown provider '#{ name }'"
       end
     end
 
     def register(provider_hash)
-      providers.merge! provider_hash
+      providers.merge! symbolize_keys(provider_hash)
     end
 
     private
     def providers
       @providers ||= {}
+    end
+
+    def symbolize_keys(hash)
+      hash.inject({}){|result, (key, value)|
+        new_key = case key
+                  when String then key.to_sym
+                  else key
+                  end
+        new_value = case value
+                    when Hash then symbolize_keys(value)
+                    else value
+                    end
+        result[new_key] = new_value
+        result
+      }
     end
   end
 end
