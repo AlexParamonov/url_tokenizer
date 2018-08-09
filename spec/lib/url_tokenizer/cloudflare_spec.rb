95c53f84e77c4008ac7f6971b0dd8b4a2ad55c35
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2018-08-09 18:24:01 +0200

    Add cloudflare token

---
+++ spec/lib/url_tokenizer/cloudflare_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+require 'url_tokenizer/cloudflare'
+require_relative 'provider_examples'
+
+describe UrlTokenizer::Cloudflare do
+  it_behaves_like :provider
+
+  subject { described_class.new key }
+  let(:key) { "secret" }
+  let(:url) { url_with_params }
+
+  it "adds verify parameter" do
+    expect(subject.call url).to include 'verify='
+  end
+
+  describe 'when url have the query string' do
+    let(:url) { url_with_params foo: :bar }
+
+    it 'does not add question mark' do
+      expect(subject.call(url).count('?')).to eq 1
+    end
+
+    it 'persists query string' do
+      expect(subject.call url).to include "foo=bar"
+    end
+  end
+
+  describe 'when url have provider parameters' do
+    let(:url) { url_with_params verify: '0000000000_e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4' }
+
+    it 'ignores and overwrites them' do
+      expect(subject.call url, expires_in: 10).not_to include '0000000000_'
+      expect(subject.call url, expires_in: 10).to match /verify=\d{10,}_/
+    end
+  end
+
+  describe 'with expiration time' do
+    it "adds timestamp" do
+      expect(subject.call url, expires_in: 123).to match /\=\d+_/
+    end
+  end
+
+  describe 'when requesting verify for domain, not for url' do
+    it 'returns nil' do
+      url = 'http://liveplay.example.com/'
+      expect(subject.call url).to be_nil
+
+      url = 'http://liveplay.example.com'
+      expect(subject.call url).to be_nil
+    end
+  end
+
+  describe 'global options' do
+    subject { described_class.new key, expires_in: 12345 }
+
+    it "uses global options" do
+      global_expiration_time = (Time.now.utc + 12345).to_i
+      actual_expiration_time = subject.call(url).match(/\=(\d+)/)[1].to_i
+      expect(actual_expiration_time).to be_within(2).of(global_expiration_time)
+    end
+
+    it "prefers local options" do
+      local_expiration_time = (Time.now.utc + 10).to_i
+
+      actual_expiration_time = subject.call(url, expires_in: 10).match(/\=(\d+)/)[1].to_i
+      expect(actual_expiration_time).to be_within(2).of(local_expiration_time)
+    end
+  end
+
+  private
+
+  def url_with_params(**params)
+    query_string = URI.encode_www_form params
+    query_string = "?#{ query_string }" unless query_string.empty?
+    "http://token.livehd.example.com/hdliveedge00/test.stream/playlist.m3u8#{ query_string }"
+  end
+end
